646cdab6aaf82585dcd602fb603171edc2f50810
Misha <mkaravaev@gmail.com>
2019-09-02 16:20:23 +0500

    tests refactoring

--- test/ex_brackets_test.exs
+++ test/ex_brackets_test.exs
@@ -1,30 +1,32 @@
 defmodule ExBracketsTest do
   use ExUnit.Case
+  @false_input [
+        ")(abcdifghigklmonop)", 
+        "(((abcdifghigklmonop))",
+        "())",
+        ")",
+        ""
+      ]
+
+  @true_input [
+    "(abcdifghigklmonop)",
+    "(((abcdifghigklmonop)))",
+    "()"
+  ]
 
   describe "&check/1" do
     test "should return true if balanced" do
-      str1 = "(abcdifghigklmonop)"
-      str2 = "(((abcdifghigklmonop)))"
-      str3 = "()"
+      Enum.map(@true_input, &(
+        assert ExBrackets.check(&1) == true
+      ))
 
-      assert ExBrackets.check(str1) == true
-      assert ExBrackets.check(str2) == true
-      assert ExBrackets.check(str3) == true
     end
 
     test "should return false if unbalanced" do
-      str1 = ")(abcdifghigklmonop)"
-      str2 = "(((abcdifghigklmonop))"
-      str3 = "())"
-      str4 = ")"
-      str5 = ""
-      assert ExBrackets.check(str1) == false
-      assert ExBrackets.check(str2) == false
-      assert ExBrackets.check(str3) == false
-      assert ExBrackets.check(str4) == false
-      assert ExBrackets.check(str5) == false
+      Enum.map(@false_input, &(
+        assert ExBrackets.check(&1) == false
+      ))
     end
-
   end
 
 end
